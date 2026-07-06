88831a82ae133c1914496387d0359c1245ee6286
Younes Hashlamoun <Younes.Engineer@Gmail.com>
2026-07-05 23:55:55 -0500

    learning section6 is finished-docker compose capabilities

---
+++ section6/jobportal/src/main/java/com/moltentech/jobportal/repository/CompanyRepository.java
@@ -0,0 +1,9 @@
+package com.moltentech.jobportal.repository;
+
+import com.moltentech.jobportal.entity.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<Company, Long> {
+}
